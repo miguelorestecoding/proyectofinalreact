2db115a5042740c3e16047684c78f6156fe6d3b2
miguelorestecoding <miguelorestecoding@gmail.com>
2023-05-20 14:03:36 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw",
+        }}
+      ></div>
+    </div>
+  );
+}
+
+export default App;
