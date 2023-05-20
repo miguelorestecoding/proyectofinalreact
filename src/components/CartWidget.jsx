2e6b14ebb194b2612e9c5c9e012d93051e5be322
miguelorestecoding <miguelorestecoding@gmail.com>
2023-05-20 17:04:40 -0300

    alineacion de CartWidget

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -4,7 +4,14 @@ import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const CartWidget = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "40px",
+      }}
+    >
       <FontAwesomeIcon icon={faCartShopping} />
       <span>7</span>
     </div>
