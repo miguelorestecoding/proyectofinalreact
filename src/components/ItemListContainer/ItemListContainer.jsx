6f1989869154a649554af7f78070bfc6b29ee69f
miguelorestecoding <miguelorestecoding@gmail.com>
2023-06-10 14:02:40 -0300

    agrego parametor categoryId al useEffect de ItemListcontainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -11,7 +11,7 @@ const ItemListContainer = ({ greeting }) => {
   const { categoryId } = useParams()
 
   useEffect(() => {
-      const asyncFunc = categoryId ? getProductsByCategory : getProducts
+      const asyncFunc = categoryId ? getProductsByCategory(categoryId) : getProducts
 
       asyncFunc(categoryId)
       .then(response => {
