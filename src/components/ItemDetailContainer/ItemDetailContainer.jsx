3d5d3dcc1de3ba9843f37475d16936fc1b0e2dab
miguelorestecoding <miguelorestecoding@gmail.com>
2023-07-01 08:24:53 -0300

    siguiendo clase. Incluye clase Firebase I. No funciona el contador.

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import { getProductById } from '../../asyncMock'
 import ItemDetail from '../ItemDetail/ItemDetail'
 import { useParams } from 'react-router-dom'
+import {doc, getDoc, getFirestore } from 'firebase/firestore'
 
 const ItemDetailContainer = () => {
     const [product, setProduct] = useState(null)
@@ -10,13 +11,22 @@ const ItemDetailContainer = () => {
     const { itemId } = useParams()
 
     useEffect(() => {
-            getProductById(itemId)
-            .then((response) => {
-                setProduct(response)
-            })
-            .catch(error => {
-                console.error(error)
-            })
+
+      const db = getFirestore();
+ const productCollection = doc(db, "proyecto-final-react", itemId);
+
+ getDoc(productCollection).then((snapshot) => {
+  setProduct({ id: snapshot.id, ...snapshot.data() }); 
+  }); 
+
+            // getProductById(itemId)
+            // .then((response) => {
+            //     setProduct(response)
+            // })
+            // .catch(error => {
+            //     console.error(error)
+            // })
+
         }, [itemId])
 
   return (
